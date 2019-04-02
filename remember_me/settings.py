34e5f43feefe90e71b3b536556ff9ed386481d88
knight7024 <knight7024@naver.com>
2019-04-02 20:01:36 +0900

    heroku commit

--- remember_me/settings.py
+++ remember_me/settings.py
@@ -147,6 +147,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'remember_me', 'static')
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
