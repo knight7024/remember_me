d4595231baa1a8fa5fefe1d84571bf51963f320a
knight7024 <knight7024@naver.com>
2019-04-02 20:12:27 +0900

    heroku commit

--- remember_me/settings.py
+++ remember_me/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorag'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
