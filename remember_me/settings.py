bb03bc4bcdc74a6b0a935773b227a3758d1a3c12
knight7024 <knight7024@naver.com>
2019-04-02 19:07:42 +0900

    heroku commit

--- remember_me/settings.py
+++ remember_me/settings.py
@@ -58,7 +58,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
